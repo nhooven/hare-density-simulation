a11585883fbf5b4d10966c876ba40b31e83d1ee5
nhooven <nathan.d.hooven@gmail.com>
2025-03-31 17:23:06 -0700

    Overhauled simulation workflow - I decided that I would like ~1000 iterations with everything being self-contained within. I can just subsample those 50 tracks. Ideally I would split these up so I can run in "parallel" on the lab desktop (this will be critical for the CTSP modeling too since that will take much longer. Consider splitting the dataset into, say, 5 folds of 200 iterations each

---
+++ 03 - Sample movement parameters.R
@@ -0,0 +1,314 @@
+# Project: WSU Snowshoe Hare and PCT Project
+# Subproject: Density - movement simulation
+# Script: 03 - Sample movement parameters
+# Author: Nathan D. Hooven, Graduate Research Assistant
+# Email: nathan.hooven@wsu.edu / nathan.d.hooven@gmail.com
+# Date began: 25 Mar 2025
+# Date completed: 25 Mar 2025
+# Date last modified: 31 Mar 2025
+# R version: 4.4.3
+
+#_______________________________________________________________________
+# 1. Load in packages ----
+#_______________________________________________________________________
+
+library(tidyverse)            # data cleaning and manipulation
+library(sf)                   # spatial operations
+library(mefa4)
+library(ctmm)                 # helper function for unit conversion
+
+#_______________________________________________________________________
+# 2. Define numbers for simulations ----
+#_______________________________________________________________________
+
+# number of iterations
+n.iter <- 1000
+
+# total individuals to simulate per iteration
+# camera contacts for Q1
+n.indiv.cam.Q1 <- 50
+
+# additional out of sample collars for Q1
+n.indiv.collar.Q1 <- 10
+
+# high variability camera contacts for Q2
+n.indiv.cam.Q2 <- 10
+
+# high variability additional out of sample collars for Q2
+n.indiv.collar.Q2 <- 5
+
+#_______________________________________________________________________
+# 3. Sample individuals ----
+#_______________________________________________________________________
+# 3a. Q1 camera ----
+#_______________________________________________________________________
+
+indivs.cam.Q1 <- expand.grid(iter = 1:n.iter,
+                             indiv = 1:n.indiv.cam.Q1,
+                             Q = "Q1",
+                             use = "camera")
+
+#_______________________________________________________________________
+# 3b. Q1 collar ----
+#_______________________________________________________________________
+
+indivs.collar.Q1 <- expand.grid(iter = 1:n.iter,
+                                indiv = 1:n.indiv.collar.Q1,
+                                Q = "Q1",
+                                use = "collar")
+
+#_______________________________________________________________________
+# 3c. Q2 camera ----
+#_______________________________________________________________________
+
+indivs.cam.Q2 <- expand.grid(iter = 1:n.iter,
+                             indiv = 1:n.indiv.cam.Q2,
+                             Q = "Q2",
+                             use = "camera")
+
+#_______________________________________________________________________
+# 3d. Q2 collar ----
+#_______________________________________________________________________
+
+indivs.collar.Q2 <- expand.grid(iter = 1:n.iter,
+                                indiv = 1:n.indiv.collar.Q2,
+                                Q = "Q2",
+                                use = "collar")
+
+#_______________________________________________________________________
+# 4. Define distributions of movement parameters ----
+
+# log-normal parameter function
+log_norm_params <- function (
+    
+  vals    # must be a length 2 vector with the desired mean and SD
+  
+) {
+  
+  # median = geometric mean
+  lnorm.med <- log(vals[1])
+  
+  lnorm.mean <- log((vals[1]^2) / sqrt((vals[1]^2) + (vals[2]^2)))
+  
+  lnorm.sd <- sqrt(log(1 + ((vals[2]^2) / (vals[1]^2))))
+  
+  # bind together
+  lnorm.params <- c(lnorm.med, lnorm.mean, lnorm.sd)
+  
+  # return
+  return(lnorm.params)
+  
+}
+
+#_______________________________________________________________________
+# 4a. Home range centroids ----
+
+# here, our definition of population density will be n home range centroids
+# (activity centers, etc.) per a 10-ha area
+
+# is this the most realistic? It's an assumption the SECR uses, and we can 
+# model it pretty intuitively with the mean parameters in ctmm
+
+# each individual will get a random uniform draw within the unit boundary, for each x and y
+
+#_______________________________________________________________________
+
+unit.bound <- st_read(paste0(getwd(), "/Derived data/Shapefiles/unit_bound.shp"))
+
+# min and max x/y
+coord.min <- as.numeric(st_bbox(unit.bound)[1])
+coord.max <- as.numeric(st_bbox(unit.bound)[3])
+
+# Q1
+indivs.cam.Q1$mean1  <- runif(n = nrow(indivs.cam.Q1), coord.min, coord.max)
+indivs.cam.Q1$mean2  <- runif(n = nrow(indivs.cam.Q1), coord.min, coord.max)
+
+indivs.collar.Q1$mean1 <- runif(n = nrow(indivs.collar.Q1), coord.min, coord.max)
+indivs.collar.Q1$mean2 <- runif(n = nrow(indivs.collar.Q1), coord.min, coord.max)
+
+# Q2
+indivs.cam.Q2$mean1  <- runif(n = nrow(indivs.cam.Q2), coord.min, coord.max)
+indivs.cam.Q2$mean2  <- runif(n = nrow(indivs.cam.Q2), coord.min, coord.max)
+
+indivs.collar.Q2$mean1 <- runif(n = nrow(indivs.collar.Q2), coord.min, coord.max)
+indivs.collar.Q2$mean2 <- runif(n = nrow(indivs.collar.Q2), coord.min, coord.max)
+
+#_______________________________________________________________________
+# 4b. Tau 1 ----
+
+# geometric mean = 6 hr
+# SDs = 1.5, 6 hr
+
+lnorm.tau1.lo <- log_norm_params(c(6 %#% "hours", 1.5 %#% "hours"))
+lnorm.tau1.hi <- log_norm_params(c(6 %#% "hours", 6 %#% "hours"))
+
+#_______________________________________________________________________
+
+# Q1
+indivs.cam.Q1$tau1 <- rlnorm(nrow(indivs.cam.Q1), 
+                             meanlog = lnorm.tau1.lo[1],
+                             sdlog = lnorm.tau1.lo[3])
+
+indivs.collar.Q1$tau1 <- rlnorm(nrow(indivs.collar.Q1), 
+                                meanlog = lnorm.tau1.lo[1],
+                                sdlog = lnorm.tau1.lo[3])
+
+# Q2
+indivs.cam.Q2$tau1 <- rlnorm(nrow(indivs.cam.Q2), 
+                             meanlog = lnorm.tau1.lo[1],
+                             sdlog = lnorm.tau1.lo[3])
+
+indivs.collar.Q2$tau1 <- rlnorm(nrow(indivs.collar.Q2), 
+                                meanlog = lnorm.tau1.lo[1],
+                                sdlog = lnorm.tau1.lo[3])
+
+#_______________________________________________________________________
+# 4c. Tau 2 ----
+
+# geometric mean = 2 hr
+# SDs = 0.5, 2 hr
+
+lnorm.tau2.lo <- log_norm_params(c(2 %#% "hours", 0.5 %#% "hours"))
+lnorm.tau2.hi <- log_norm_params(c(2 %#% "hours", 2 %#% "hours"))
+
+#_______________________________________________________________________
+
+# Q1
+indivs.cam.Q1$tau2 <- rlnorm(nrow(indivs.cam.Q1), 
+                             meanlog = lnorm.tau2.lo[1],
+                             sdlog = lnorm.tau2.lo[3])
+
+indivs.collar.Q1$tau2 <- rlnorm(nrow(indivs.collar.Q1), 
+                                meanlog = lnorm.tau2.lo[1],
+                                sdlog = lnorm.tau2.lo[3])
+
+# Q2
+indivs.cam.Q2$tau2 <- rlnorm(nrow(indivs.cam.Q2), 
+                             meanlog = lnorm.tau2.hi[1],
+                             sdlog = lnorm.tau2.hi[3])
+
+indivs.collar.Q2$tau2 <- rlnorm(nrow(indivs.collar.Q2), 
+                                meanlog = lnorm.tau2.hi[1],
+                                sdlog = lnorm.tau2.hi[3])
+
+#_______________________________________________________________________
+# 4d. Sigma ----
+
+# sigma major
+# geometric mean = 10,000 m
+# SDs = 2,500, 10,000 m
+
+lnorm.sigma.maj.lo <- log_norm_params(c(10000, 2500))
+lnorm.sigma.maj.hi <- log_norm_params(c(10000, 10000))
+
+# "aspect ratio"
+# synthetic parameter that relates the major to minor axis sigmas
+# these will also be log-normal draws, and then we'll calculate sigma minor
+lnorm.aspect.lo <- log_norm_params(c(7, 1.75))
+lnorm.aspect.hi <- log_norm_params(c(7, 7))
+
+# angle
+# this determine which axis the long dimension of the track aligns with
+# it varies between - pi / 2 and pi / 2
+
+#_______________________________________________________________________
+
+# sigma major
+# Q1
+indivs.cam.Q1$sigma.maj <- rlnorm(nrow(indivs.cam.Q1), 
+                                  meanlog = lnorm.sigma.maj.lo[1],
+                                  sdlog = lnorm.sigma.maj.lo[3])
+
+indivs.collar.Q1$sigma.maj <- rlnorm(nrow(indivs.collar.Q1), 
+                                     meanlog = lnorm.sigma.maj.lo[1],
+                                     sdlog = lnorm.sigma.maj.lo[3])
+
+# Q2
+indivs.cam.Q2$sigma.maj <- rlnorm(nrow(indivs.cam.Q2), 
+                                  meanlog = lnorm.sigma.maj.hi[1],
+                                  sdlog = lnorm.sigma.maj.hi[3])
+
+indivs.collar.Q2$sigma.maj <- rlnorm(nrow(indivs.collar.Q2), 
+                                     meanlog = lnorm.sigma.maj.hi[1],
+                                     sdlog = lnorm.sigma.maj.hi[3])
+
+# aspect ratio
+# Q1
+indivs.cam.Q1$aspect <- rlnorm(nrow(indivs.cam.Q1), 
+                               meanlog = lnorm.aspect.lo[1],
+                               sdlog = lnorm.aspect.lo[3])
+
+indivs.collar.Q1$aspect <- rlnorm(nrow(indivs.collar.Q1), 
+                                  meanlog = lnorm.aspect.lo[1],
+                                  sdlog = lnorm.aspect.lo[3])
+
+# Q2
+indivs.cam.Q2$aspect <- rlnorm(nrow(indivs.cam.Q2), 
+                               meanlog = lnorm.aspect.hi[1],
+                               sdlog = lnorm.aspect.hi[3])
+
+indivs.collar.Q2$aspect <- rlnorm(nrow(indivs.collar.Q2), 
+                                  meanlog = lnorm.aspect.hi[1],
+                                  sdlog = lnorm.aspect.hi[3])
+
+# calculate sigma minor
+indivs.cam.Q1$sigma.min         <- indivs.cam.Q1$sigma.maj / indivs.cam.Q1$aspect
+indivs.collar.Q1$sigma.min      <- indivs.collar.Q1$sigma.maj / indivs.collar.Q1$aspect
+indivs.cam.Q2$sigma.min         <- indivs.cam.Q2$sigma.maj / indivs.cam.Q2$aspect
+indivs.collar.Q2$sigma.min      <- indivs.collar.Q2$sigma.maj / indivs.collar.Q2$aspect
+
+# angle
+indivs.cam.Q1$angle            <- runif(n = nrow(indivs.cam.Q1), -pi / 2, pi /2)
+indivs.collar.Q1$angle         <- runif(n = nrow(indivs.collar.Q1), -pi / 2, pi /2)
+indivs.cam.Q2$angle            <- runif(n = nrow(indivs.cam.Q2), -pi / 2, pi /2)
+indivs.collar.Q2$angle         <- runif(n = nrow(indivs.collar.Q2), -pi / 2, pi /2)
+
+#_______________________________________________________________________
+# 5. Final check ----
+
+# here we should make absolute certain that Tau2 is never > Tau1
+# a while loop might be necessary here
+
+#_______________________________________________________________________
+
+# how many in each?
+sum(indivs.cam.Q1$tau2 > indivs.cam.Q1$tau1)
+sum(indivs.collar.Q1$tau2 > indivs.collar.Q1$tau1)
+sum(indivs.cam.Q2$tau2 > indivs.cam.Q2$tau1)
+sum(indivs.collar.Q2$tau2 > indivs.collar.Q2$tau1)
+
+# replace with a new draw while there are still cases
+fix_tau2 <- function(df) {
+  
+  while (sum(df$tau2 > df$tau1) > 0) {
+    
+    df$tau2[which(df$tau2 > df$tau1)] <- rlnorm(length(df$tau2[which(df$tau2 > df$tau1)]), 
+                                                meanlog = lnorm.tau2.lo[1],
+                                                sdlog = lnorm.tau2.lo[3])
+    
+  } 
+    
+  return(df)
+    
+}
+
+# use function
+indivs.cam.Q1 <- fix_tau2(indivs.cam.Q1)
+indivs.collar.Q1 <- fix_tau2(indivs.collar.Q1)
+indivs.cam.Q2 <- fix_tau2(indivs.cam.Q2)
+indivs.collar.Q2 <- fix_tau2(indivs.collar.Q2)
+
+# did I fix it?
+sum(indivs.cam.Q1$tau2 > indivs.cam.Q1$tau1)
+sum(indivs.collar.Q1$tau2 > indivs.collar.Q1$tau1)
+sum(indivs.cam.Q2$tau2 > indivs.cam.Q2$tau1)
+sum(indivs.collar.Q2$tau2 > indivs.collar.Q2$tau1)
+
+#_______________________________________________________________________
+# 6. Write to .csvs ----
+#_______________________________________________________________________
+
+write.csv(indivs.cam.Q1, paste0(getwd(), "/Derived data/Parameters/camera_lo.csv"))
+write.csv(indivs.collar.Q1, paste0(getwd(), "/Derived data/Parameters/collar_lo.csv"))
+write.csv(indivs.cam.Q2, paste0(getwd(), "/Derived data/Parameters/camera_hi.csv"))
+write.csv(indivs.collar.Q2, paste0(getwd(), "/Derived data/Parameters/collar_hi.csv"))
---
+++ 04 - CTMM simulations.R
@@ -0,0 +1,353 @@
+# Project: WSU Snowshoe Hare and PCT Project
+# Subproject: Density - movement simulation
+# Script: 04 - CTMM simulations
+# Author: Nathan D. Hooven, Graduate Research Assistant
+# Email: nathan.hooven@wsu.edu / nathan.d.hooven@gmail.com
+# Date began: 25 Mar 2025
+# Date completed: 26 Mar 2025
+# Date last modified: 31 Mar 2025
+# R version: 4.4.3
+
+#_______________________________________________________________________
+# 1. Load in packages ----
+#_______________________________________________________________________
+
+library(tidyverse)            # data cleaning and manipulation
+library(lubridate)            # work with dates
+library(ctmm)                 # CTSP movement modeling
+library(amt)                  # work with animal movement tracks
+library(sf)                   # spatial operations
+
+#_______________________________________________________________________
+# 2. Read in data ----
+#_______________________________________________________________________
+
+# movement parameters
+Q1.cam <- read.csv(paste0(getwd(), "/Derived data/Parameters/camera_lo.csv"))
+Q1.col <- read.csv(paste0(getwd(), "/Derived data/Parameters/collar_lo.csv"))
+Q2.cam <- read.csv(paste0(getwd(), "/Derived data/Parameters/camera_hi.csv"))
+Q2.col <- read.csv(paste0(getwd(), "/Derived data/Parameters/collar_hi.csv"))
+
+# unit boundary
+unit.bound <- st_read(paste0(getwd(), "/Derived data/Shapefiles/unit_bound.shp"))
+
+# camera viewsheds
+vs <- st_read(paste0(getwd(), "/Derived data/Shapefiles/cams_vs.shp"))
+
+vs$cam.id <- 1:nrow(vs)
+
+st_crs(vs) <- "epsg:32611"
+
+#_______________________________________________________________________
+# 3. Simulation parameters ----
+
+# all camera contact sims will last 4 weeks and will be sampled at a 60-sec "fundamental step"
+
+#_______________________________________________________________________
+
+# simulation duration
+sim.duration.wk <- 4
+
+sim.duration.sec <- sim.duration.wk * 7 * 24 * 60 * 60 
+
+# "sampling rate" for fundamental (i.e., straight line) steps
+sim.samp.rate <- 60
+
+# time steps to simulate on (from, to, by)
+sim.timestep <- seq(1, sim.duration.sec, sim.samp.rate)
+
+# collar sims ONLY
+sim.duration.hr <- sim.duration.wk * 7 * 24
+
+sim.timestep.hr <- seq(1, sim.duration.sec, 3600)
+
+#_______________________________________________________________________
+# 4. Loop ----
+
+# number of iterations (let's stop calling them replicates)
+n.iter <- 1
+
+#_______________________________________________________________________
+# 4a. Define functions ----
+#_______________________________________________________________________
+
+# simulations for camera contacts
+cam_sim_contact <- function (focal.row) {
+  
+  # output list (will hold 3 dfs)
+  focal.list <- list()
+  
+  # define OUF model
+  ouf.mod <- ctmm(tau = c(focal.row$tau1,             # positional autocorr time
+                          focal.row$tau2),            # velocity autocorr time
+                  isotropic = FALSE,                  # anisotropic
+                  sigma = c(focal.row$sigma.maj,      # variance along major axis
+                            focal.row$sigma.min,      # variance along minor axis
+                            focal.row$angle),         # angle of axis
+                  mu = c(focal.row$mean1,             # x coord
+                         focal.row$mean2))            # y coord
+  
+  # run simulation
+  ouf.sim <- simulate(object = ouf.mod, 
+                      t = sim.timestep,
+                      complete = T)
+  
+  # coerce telemetry object to sf
+  telem.df <- data.frame(t = ouf.sim$t,
+                         x = ouf.sim$x,
+                         y = ouf.sim$y,
+                         timestamp = as.POSIXct(ouf.sim$timestamp))
+  
+  telem.sf <- st_as_sf(telem.df,
+                       coords = c("x", "y"),
+                       crs = "epsg:32611")
+  
+  # tally camera contacts ("passes")
+  # in this case, 60-second locations that fall within each viewshed
+  # these will be independent detections (> 20 minutes apart)
+  passes <- st_intersection(vs, 
+                            telem.sf) %>%
+    
+    # drop the geometry
+    st_drop_geometry() %>% 
+    
+    # group by camera
+    group_by(cam.id) %>% 
+    
+    # add a time difference column
+    mutate(time.diff = as.numeric(t - lag(t))) %>%
+    
+    # drop anything < 20 minutes
+    filter(time.diff > 20 %#% "minutes") %>%
+    
+    # tally all intersections
+    tally() %>%
+    
+    # and add identifiers
+    mutate(iter = focal.row$iter,
+           indiv = focal.row$indiv,
+           Q = focal.row$Q,
+           use = focal.row$use)
+  
+  # sample to a "GPS track"
+  # this will be a regular 1-hr track, which can be subsampled
+  telem.track <- telem.df %>%
+    
+    make_track(.x = x,
+               .y = y,
+               .t = timestamp)
+  
+  # calculate "true" speed (m/s)
+  telem.track.speed <- telem.track %>%
+    
+    steps() 
+  
+  # 4 week
+  speed.4wk <- sum(telem.track.speed$sl_) / sim.duration.sec
+  
+  # resample track for telemetering
+  telem.track.1 <- telem.track %>%
+    
+    track_resample(rate = hours(1),
+                   tolerance = minutes(0)) %>%
+    
+    # remove "burst"
+    dplyr::select(-burst_) %>%
+    
+    # and add identifiers
+    mutate(iter = focal.row$iter,
+           indiv = focal.row$indiv,
+           Q = focal.row$Q,
+           use = focal.row$use)
+  
+  # speeds df
+  focal.speeds <- data.frame(iter = focal.row$iter,
+                             indiv = focal.row$indiv,
+                             Q = focal.row$Q,
+                             use = focal.row$use,
+                             speed.4wk = speed.4wk)
+  
+  # bind each into list
+  focal.list[[1]] <- passes
+  focal.list[[2]] <- telem.track.1
+  focal.list[[3]] <- focal.speeds
+  
+  # return
+  return(focal.list)
+  
+}
+
+# simulations for collared individuals only
+col_sim <- function (focal.row) {
+  
+  # output list (will hold 3 dfs)
+  focal.list <- list()
+  
+  # define OUF model
+  ouf.mod <- ctmm(tau = c(focal.row$tau1,             # positional autocorr time
+                          focal.row$tau2),            # velocity autocorr time
+                  isotropic = FALSE,                  # anisotropic
+                  sigma = c(focal.row$sigma.maj,      # variance along major axis
+                            focal.row$sigma.min,      # variance along minor axis
+                            focal.row$angle),         # angle of axis
+                  mu = c(focal.row$mean1,             # x coord
+                         focal.row$mean2))            # y coord
+  
+  # run simulation
+  ouf.sim <- simulate(object = ouf.mod, 
+                      t = sim.timestep.hr,
+                      complete = T)
+  
+  # coerce telemetry object to sf
+  telem.df <- data.frame(t = ouf.sim$t,
+                         x = ouf.sim$x,
+                         y = ouf.sim$y,
+                         timestamp = as.POSIXct(ouf.sim$timestamp))
+  
+  telem.sf <- st_as_sf(telem.df,
+                       coords = c("x", "y"),
+                       crs = "epsg:32611")
+  
+  # convert to a "GPS track"
+  # this will be a regular 1-hr track
+  telem.track <- telem.df %>%
+    
+    make_track(.x = x,
+               .y = y,
+               .t = timestamp) %>%
+    
+    # and add identifiers
+    mutate(iter = focal.row$iter,
+           indiv = focal.row$indiv,
+           Q = focal.row$Q,
+           use = focal.row$use)
+  
+  # return
+  return(telem.track)
+  
+}
+
+#_______________________________________________________________________
+# 4b. Initialize dfs ----
+#_______________________________________________________________________
+
+Q1.cam.all.passes <- data.frame()
+Q1.cam.all.relocs <- data.frame()
+Q1.cam.all.speeds <- data.frame()
+
+Q1.col.all.relocs <- data.frame()
+
+Q2.cam.all.passes <- data.frame()
+Q2.cam.all.relocs <- data.frame()
+Q2.cam.all.speeds <- data.frame()
+
+Q2.col.all.relocs <- data.frame()
+
+#_______________________________________________________________________
+# 4c. Run loop ----
+#_______________________________________________________________________
+
+# loop through all iterations (i)
+
+# start time
+start.time <- Sys.time()
+
+for (i in 1:n.iter) {
+  
+  # subset iteration from each df
+  focal.Q1.cam <- Q1.cam %>% filter(iter == i)
+  focal.Q1.col <- Q1.col %>% filter(iter == i)
+  focal.Q2.cam <- Q2.cam %>% filter(iter == i)
+  focal.Q2.col <- Q2.col %>% filter(iter == i)
+  
+  # loop through all individuals, by dataset (w, x, y, z)
+  # Q1.cam (w)
+  for (w in 1:nrow(focal.Q1.cam)) {
+    
+    # subset row
+    focal.row.w <- focal.Q1.cam %>% slice(w)
+    
+    # use function
+    focal.sims.w <- cam_sim_contact(focal.row.w)
+    
+    # bind in
+    Q1.cam.all.passes <- rbind(Q1.cam.all.passes, focal.sims.w[[1]])
+    Q1.cam.all.relocs <- rbind(Q1.cam.all.relocs, focal.sims.w[[2]])
+    Q1.cam.all.speeds <- rbind(Q1.cam.all.speeds, focal.sims.w[[3]])
+    
+  }
+  
+  # Q1.col (x)
+  for (x in 1:nrow(focal.Q1.col)) {
+    
+    # subset row
+    focal.row.x <- focal.Q1.col %>% slice(x)
+    
+    # use function
+    focal.sims.x <- col_sim(focal.row.x)
+    
+    # bind in
+    Q1.col.all.relocs <- rbind(Q1.col.all.relocs, focal.sims.x)
+    
+  }
+  
+  # Q2.cam (y)
+  for (y in 1:nrow(focal.Q2.cam)) {
+    
+    # subset row
+    focal.row.y <- focal.Q2.cam %>% slice(y)
+    
+    # use function
+    focal.sims.y <- cam_sim_contact(focal.row.y)
+    
+    # bind in
+    Q2.cam.all.passes <- rbind(Q1.cam.all.passes, focal.sims.y[[1]])
+    Q2.cam.all.relocs <- rbind(Q1.cam.all.relocs, focal.sims.y[[2]])
+    Q2.cam.all.speeds <- rbind(Q1.cam.all.speeds, focal.sims.y[[3]])
+    
+  }
+  
+  # Q2.col (z)
+  for (z in 1:nrow(focal.Q2.col)) {
+    
+    # subset row
+    focal.row.z <- focal.Q2.col %>% slice(z)
+    
+    # use function
+    focal.sims.z <- col_sim(focal.row.z)
+    
+    # bind in
+    Q2.col.all.relocs <- rbind(Q2.col.all.relocs, focal.sims.z)
+    
+  }
+  
+  # every 50 iterations:
+    # save each to file for my sanity
+    # print status message
+  
+  if (i %% 50 == 0) {
+    
+    # save each to file
+    write.csv(Q1.cam.all.passes, paste0(getwd(), "/Derived data/Camera detections/detections_lo.csv")) 
+    write.csv(Q1.cam.all.relocs, paste0(getwd(), "/Derived data/Simulated tracks/tracks_lo.csv"))
+    write.csv(Q1.cam.all.speeds, paste0(getwd(), "/Derived data/Speeds/speeds_lo.csv"))
+    
+    write.csv(Q1.col.all.relocs, paste0(getwd(), "/Derived data/Simulated tracks/tracks_col_lo.csv"))
+    
+    write.csv(Q2.cam.all.passes, paste0(getwd(), "/Derived data/Camera detections/detections_hi.csv"))
+    write.csv(Q2.cam.all.relocs, paste0(getwd(), "/Derived data/Simulated tracks/tracks_hi.csv"))
+    write.csv(Q2.cam.all.speeds, paste0(getwd(), "/Derived data/Speeds/speeds_hi.csv"))
+    
+    write.csv(Q2.col.all.relocs, paste0(getwd(), "/Derived data/Simulated tracks/tracks_col_hi.csv"))
+    
+    # print status message
+    elapsed.time <- round(as.numeric(difftime(Sys.time(), 
+                                              start.time, 
+                                              units = "mins")), 
+                          digits = 1)
+    
+    print(paste0("Completed sim ", i, " of ", n.iter, " - ", elapsed.time, " mins"))
+    
+  }
+  
+}
