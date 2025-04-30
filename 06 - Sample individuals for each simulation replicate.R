ecbcc6dcffa9315bae4bf8dfc866d9346c9d5f29
nhooven <nathan.d.hooven@gmail.com>
2025-04-30 14:41:35 -0700

    Wrote function to sample "mismatched" individuals

--- 06 - Sample individuals for each simulation replicate.R
+++ 06 - Sample individuals for each simulation replicate.R
@@ -5,7 +5,7 @@
 # Email: nathan.hooven@wsu.edu / nathan.d.hooven@gmail.com
 # Date began: 28 Apr 2025
 # Date completed: 28 Apr 2025
-# Date last modified: 28 Apr 2025
+# Date last modified: 30 Apr 2025
 # R version: 4.4.3
 
 #_______________________________________________________________________
@@ -139,7 +139,112 @@ sampled.indiv.reps.1 <- sample_indiv_reps(1000, 1)
 sampled.indiv.reps.2 <- sample_indiv_reps(1000, 2)
 
 #_______________________________________________________________________
-# 5. Write to files ----
+# 5. Question 3 ----
+
+# Here we'll sample camera individuals as usual, but
+# sample collared individuals weighted by their speed
+# this emulates collaring individuals with a more restless movement syndrome
+# than the population on average
+
+# read in "true speeds" - must be the highly variable subset
+speeds.2T <- read.csv(paste0(getwd(), "/Derived data/Sampled - Speeds/speeds_2T.csv"))
+speeds.2NT <- read.csv(paste0(getwd(), "/Derived data/Sampled - Speeds/speeds_2NT.csv"))
+
+hist(speeds.2T$true.speed)
+hist(speeds.2NT$true.speed)
+
+#_______________________________________________________________________
+# 5a. Write function ----
+#_______________________________________________________________________
+
+sample_indiv_reps_q3 <- function (n.rep = 1000) {
+  
+  # loop
+  all.contact.df <- data.frame()
+  all.col.df <- data.frame()
+  
+  for (i in 1:n.rep) {
+    
+    # available individuals
+    indiv.ids.T <- speeds.2T$indiv
+    
+    # sample targets
+    T.32 <- sample(indiv.ids.T, size = 32, replace = FALSE)
+    T.16 <- sample(T.32, size = 16, replace = FALSE)
+    T.08 <- sample(T.16, size = 8, replace = FALSE)
+    T.04 <- sample(T.08, size = 4, replace = FALSE)
+    
+    # sample non-targets (must be twice the targets)
+    NT.32 <- sample(indiv.ids.T, size = 32 * 2, replace = FALSE)
+    NT.16 <- sample(NT.32, size = 16 * 2, replace = FALSE)
+    NT.08 <- sample(NT.16, size = 8 * 2, replace = FALSE)
+    NT.04 <- sample(NT.08, size = 4 * 2, replace = FALSE)
+    
+    # sample  collared individuals
+    # we'll take six from the targets and 6 from the non-targets, weighted by speed
+    extra.col.T <- sample(indiv.ids.T, size = 6, replace = FALSE, prob = speeds.2T$true.speed)
+    extra.col.NT <- sample(indiv.ids.T, size = 6, replace = FALSE, prob = speeds.2NT$true.speed)
+    
+    # create dfs
+    # targets
+    T.df <- data.frame(q = 3,
+                       rep = i,
+                       purpose = "T",
+                       indiv = c(T.32,
+                                 T.16,
+                                 T.08,
+                                 T.04),
+                       abund = c(rep(32, times = 32),
+                                 rep(16, times = 16),
+                                 rep(8, times = 8),
+                                 rep(4, times = 4)))
+    
+    # non-targets
+    NT.df <- data.frame(q = 3,
+                        rep = i,
+                        purpose = "NT",
+                        indiv = c(NT.32,
+                                  NT.16,
+                                  NT.08,
+                                  NT.04),
+                        abund = c(rep(32, times = 64),
+                                  rep(16, times = 32),
+                                  rep(8, times = 16),
+                                  rep(4, times = 8)))
+    
+    # collared individuals
+    col.df <- data.frame(q = 3,
+                         rep = i,
+                         purpose = "col",
+                         indiv = c(extra.col.T,
+                                   extra.col.NT),
+                         source = c(rep("T", times = 6),
+                                    rep("NT", times = 6)))
+    
+    # bind contact individuals together
+    contact.df <- rbind(T.df, NT.df)
+    
+    # bind both into master df
+    all.contact.df <- rbind(all.contact.df, contact.df)
+    all.col.df <- rbind(all.col.df, col.df)
+    
+  }
+  
+  # bind into list and return
+  both.df <- list(all.contact.df, all.col.df)
+  
+  return(both.df)
+  
+}
+
+#_______________________________________________________________________
+# 5b. Use function ----
+#_______________________________________________________________________
+
+sampled.indiv.reps.3 <- sample_indiv_reps_q3(1000)
+
+#_______________________________________________________________________
+# 6. Write to files ----
 #_______________________________________________________________________
 
 write.csv(sampled.indiv.reps.1[[1]], file = paste0(getwd(), "/Derived data/Sampled reps/contacts_1.csv"))
@@ -147,3 +252,6 @@ write.csv(sampled.indiv.reps.1[[2]], file = paste0(getwd(), "/Derived data/Sampl
 
 write.csv(sampled.indiv.reps.2[[1]], file = paste0(getwd(), "/Derived data/Sampled reps/contacts_2.csv"))
 write.csv(sampled.indiv.reps.2[[2]], file = paste0(getwd(), "/Derived data/Sampled reps/collared_2.csv"))
+
+write.csv(sampled.indiv.reps.3[[1]], file = paste0(getwd(), "/Derived data/Sampled reps/contacts_3.csv"))
+write.csv(sampled.indiv.reps.3[[2]], file = paste0(getwd(), "/Derived data/Sampled reps/collared_3.csv"))
